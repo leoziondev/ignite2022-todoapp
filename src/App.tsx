889b397fa2f0da642e1ed43f59f13b453becb812
LeoZion <leozionsuporte@gmail.com>
2022-07-11 21:48:31 -0300

    styling home todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,105 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { CheckCircle, Circle, PlusCircle } from 'phosphor-react'
+
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import { EmptyTasks } from './components/EmptyTasks'
+import { TaskItem } from './components/TaskItem'
 
 export function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: uuidv4(),
+      title: 'Ler o escopo do desafio',
+      isComplete: false
+    },
+    {
+      id: uuidv4(),
+      title: 'Subir novo projeto ReactJS com Typescript usando Vite',
+      isComplete: false
+    },
+    {
+      id: uuidv4(),
+      title: 'Limpar arquivos desnecessários',
+      isComplete: true
+    },
+    {
+      id: uuidv4(),
+      title: 'Criar um repositório no github e fazer commit inicial',
+      isComplete: true
+    },
+    {
+      id: uuidv4(),
+      title: 'Definir estrutura HTML, CSS e definir componentes da aplicação.',
+      isComplete: false
+    },
+    {
+      id: uuidv4(),
+      title: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos odit laborum nulla impedit aut sequi quis quia nostrum neque voluptatem at sapiente, vitae error deleniti.',
+      isComplete: true
+    }
+  ])
+  const [newTask, setNewTask] = useState('')
+
+  const handleNewTaskChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value)
+  }
+
+  const handleCreateNewTask = (event: FormEvent) => {
+    event.preventDefault()
+  }
+
+  const tasksIsCompleted = tasks.filter((task) => task.isComplete === true)
+
+  const isEmptyNewTask = newTask.length === 0
 
   return (
     <>
       <Header />
+
+      <main className={styles.mainContainer}>
+        <form action="" onSubmit={handleCreateNewTask} className={styles.formContainer}>
+          <input
+            type="text"
+            value={newTask}
+            onChange={handleNewTaskChange}
+            placeholder="Adicione uma nova tarefa"
+          />
+
+          <button type="submit" disabled={isEmptyNewTask}>
+            Criar
+            <PlusCircle size={18} />
+          </button>
+        </form>
+
+        <div className={styles.statusContainer}>
+          <p>Tarefas criadas <span>{tasks.length}</span></p>
+          {tasks.length
+          ? (
+            <p>Concluídas <span>{tasksIsCompleted.length} de {tasks.length}</span></p>
+          ) : (
+            <p>Concluídas <span>0</span></p>
+          )}
+        </div>
+
+        {tasks.length
+        ? (
+          <ul>
+            {tasks.map((task) => (
+              <TaskItem
+                key={task.id}
+                isComplete={task.isComplete ? <CheckCircle size={20} weight="fill" className={styles.isComplete} /> : <Circle size={20}  />}
+                title={task.title}
+                textComplete={task.isComplete ? styles.textComplete : ''}
+              />              
+            ))}
+          </ul>
+        ) : (
+          <EmptyTasks />
+        )}
+      </main>
     </>
   )
 }
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -5,14 +5,15 @@ import styles from './TaskItem.module.css'
 interface TaskItemProps {
   title: string;
   isComplete: any;
+  textComplete: string;
 }
 
-export function TaskItem({ title, isComplete }: TaskItemProps) {
+export function TaskItem({ title, isComplete, textComplete }: TaskItemProps) {
   return (
     <li className={styles.taskItem}>
       <div>
         <button>{isComplete}</button>
-        <p>{title}</p>
+        <p className={textComplete}>{title}</p>
       </div>
       <button>
         <Trash size={18} />
